53a610ce2ffd803f83afbe34b0fb1b22896cfaf8
llucero <clucero3696@gmail.com>
2024-07-08 21:19:59 -0700

    Responsive Fix for  Header & Service Cards

--- nextjs-appointments/components/ServiceCard.js
+++ nextjs-appointments/components/ServiceCard.js
@@ -9,9 +9,9 @@ const ServiceCard = ({props}) => {
 
 
   return (
-    <Box w='50%' m='0 auto'>
+    <Box w={['95%', '95%', '50%']} m='0 auto'>
       <Box borderRadius='xl' p='1rem' backgroundColor='#C38B8B' color='white'>
-        <Text fontWeight='bold' fontSize='xl' borderBottom='1px' m='.5rem 0' borderColor='#9B5D73'>{props.service}</Text>
+        <Text fontWeight='bold' fontSize='xl' borderBottom='1px' mb='.5rem' borderColor='#9B5D73'>{props.service}</Text>
         <Box borderBottom='1px' borderColor='#9B5D73'>
           <Text w='95%' fontSize='sm' lineHeight='19px' m='0 auto' mb='.5rem' >
             {props.desc}
