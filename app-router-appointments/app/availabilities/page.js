53ba7909488f9c8f029f7ef2d73787a9e0e7591c
llucero <clucero3696@gmail.com>
2024-08-11 00:20:25 -0700

    Suspense Boundary

--- app-router-appointments/app/availabilities/page.js
+++ app-router-appointments/app/availabilities/page.js
@@ -1,14 +1,20 @@
 
 import Calander from "../ui/Calander";
 import TimeSlots from "../ui/TimeSlots";
+import { Suspense } from "react";
 
 export default function Page({ searchParams }) {
 
   return (
     <div>
       <div className='text-2xl font-bold my-8'>Select a Date & Time</div>
-      <Calander />
-      <TimeSlots params={searchParams}/>
+      <Suspense>
+        <Calander />
+      </Suspense>
+      <Suspense>
+        <TimeSlots params={searchParams}/>
+      </Suspense>
+      
     </div>
   )
 }
