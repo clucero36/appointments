2d927203c974f239e6af3ef7a6d385267fe4ba31
llucero <clucero3696@gmail.com>
2024-10-16 17:29:49 -0700

    bookable-service refactor

--- app-router-appointments/app/bookable-services/page.js
+++ app-router-appointments/app/bookable-services/page.js
@@ -1,13 +1,13 @@
 
-import Services from "../ui/bookable-services/Services";
+import Services from "@/app/ui/bookable-services/Services";
 import { Suspense } from "react";
-import { ServicesSkeleton } from "../ui/skeletons";
+import { ServicesSkeleton } from "@/app/ui/skeletons";
 
 export default function Page() {
 
   return (
     <main>
-      <div className='text-2xl font-bold mt-6 mb-5'>Select a Service</div>
+      <div className='text-2xl font-bold mt-6 mb-5'>Select Your Service & Staff</div>
       <Suspense fallback={<ServicesSkeleton />} >
         <Services />
       </Suspense>
---
+++ app-router-appointments/app/ui/bookable-services/ServiceStaff.js
@@ -0,0 +1,78 @@
+'use client'
+
+import { useState, useEffect } from "react"
+import Link from "next/link"
+import ServiceCard from "../ServiceCard";
+import { catalogData } from "@/app/lib/data";
+import { getStaff } from "@/app/lib/data";
+
+export default function ServiceStaff({ services }) {
+
+  const [staff, setStaff] = useState(null);
+  const [service, setService] = useState(null);
+  const catalogDetails = catalogData;
+
+  async function clickHandler(id) {
+    try {
+      let res = await getStaff(id);
+      setStaff(res.team);
+      setService(res.service);
+    } catch {
+      console.error("nice try simp");
+    }
+  }
+
+
+  if (staff === null && service === null) {
+    return (
+      <div className="grid grid-cols-7">
+        <div className="col-span-4">
+          {
+          services.map((service, index) => (
+              <button onClick={() => clickHandler(service.id)} key={service.id} className="px-4 py-2 w-4/5 md:w-2/3 sm:px-0 mx-auto">
+                <ServiceCard  service={service.itemData.name} description={catalogDetails[index].desc} />
+              </button>
+          ))}
+        </div>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div className="grid grid-cols-7">
+        <div className="col-span-4">
+        {
+          services.map((service, index) => (
+              <button onClick={() => clickHandler(service.id)} key={service.id} className="px-4 py-2 sm:px-0 w-4/5 md:w-2/3 mx-auto">
+                <ServiceCard  service={service.itemData.name} description={catalogDetails[index].desc} />
+              </button>
+          ))
+        }
+        </div>
+        <div className="col-span-3 w-4/5 md:w-2/3 py-2">
+        {
+          staff.map((teamMember) => {
+            return (
+              <Link className="py-4" key={teamMember.id} href={{
+                pathname: '/availabilities',
+                query: {
+                  serviceId: `${service.object.id}`,
+                  teamMemberId: `${teamMember.id}`,
+                  serviceVariationId: `${service.object.itemData.variations[0].id}`,
+                }
+              }}>
+                <div className="border border-[#9b5d73] p-2 hover:border-[#c38b8b] text-left">
+                  <div>{teamMember.givenName}</div>
+                  <div>{teamMember.familyName}</div>
+                  <div>{teamMember.emailAddress}</div>
+                  <div>{teamMember.phoneNumber}</div>
+                </div>
+              </Link>
+            )
+          })
+        }
+        </div>
+      </div>
+    )
+  }
+}
